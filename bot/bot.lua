1d25ed3d1fa4e330827cdf5d7ab9f23b274e6497
AliNort <alinort7@gmail.com>
2016-10-03 21:36:29 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -204,7 +204,7 @@ function load_config( )
     f:close()
   end
   local config = loadfile ("./data/config.lua")()
-  for v,user in pairs(config.sudo_users) do
+  for v,user in pairs(config.163120894) do
     print('\27[93mAllowed user:\27[39m ' .. user)
   end
   return config
